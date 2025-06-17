178a3c5be99bd0b532d0760683ec95c22ce905af
zzoaluu <zzoaluu@gmail.com>
2025-06-17 10:26:59 +0900

    0616 수업 3차시

--- Assets/09. Class/Test.cs
+++ Assets/09. Class/Test.cs
@@ -1,7 +1,18 @@
-using UnityEngine;
+ using UnityEngine;
 
-public class TestCol : MonoBehaviour
+public class Test : MonoBehaviour
 {
+
+    void Start()
+    {
+        Debug.Log("나는 스타트");
+    }
+
+    void Update()
+    {
+        Debug.Log("나는 업데이트");
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("온트리거엔터 충돌 감지됨: " + other.name);
@@ -12,4 +23,3 @@ public class TestCol : MonoBehaviour
     //    Debug.Log("온콜리젼엔터 충돌 감지됨: " + collision.gameObject.name);        
     //}
 }
-
